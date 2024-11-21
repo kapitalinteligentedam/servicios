73a4ebd52f1a39d58b60f5bea44bc0e1905300bc
luciiarivass <luciarivascrespo@gmail.com>
2024-11-21 19:32:22 +0100

    luciar

---
+++ esamen/lib/isolatescreen.dart
@@ -0,0 +1,69 @@
+import 'dart:isolate';
+import 'package:flutter/material.dart';
+
+class IsolateScreen extends StatefulWidget {
+  const IsolateScreen({super.key});
+
+  @override
+  State<IsolateScreen> createState() => _IsolateScreenState();
+}
+int _contador = 0;
+class _IsolateScreenState extends State<IsolateScreen> {
+  static _tareaPesada()async* {
+    int sum = 0;
+    for (int i = 0; i <= 1000000000; i++) {
+      yield sum += i;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Isolates'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.arrow_forward),
+              onPressed: () {
+                Navigator.pushNamed(context, 'stream');
+              },
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Presiona el botón para iniciar la cuenta',
+                    style: const TextStyle(fontSize: 30)),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      CircularProgressIndicator;
+                    });
+                  },
+                  child: const Text('Iniciar operación'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text('Sumar 1'),
+                  onPressed: () {
+                    setState(() {
+                      _contador++;
+                    });
+                  },
+                ),
+                Text('$_contador', style: const TextStyle(fontSize: 30))
+              ],
+            ),
+          ),
+        ));
+  }
+}
